d105784391e0b696215406b1d408a977cbb95cd2
me <me@gmail.com>
2023-12-06 09:55:07 +0100

    second commit: Create GraphQl Schema

--- server.js
+++ server.js
@@ -1,6 +1,25 @@
 const express = require('express');
 
+const { createSchema } = require('graphql-yoga');
+
+const schema = createSchema({
+    typeDefs: /* GraphQL */ `
+      type Query {
+        description: String
+        price: Float
+      }
+    `,
+    resolvers: {
+      Query: {
+        description: () => 'Red Shoe',
+        price: () => 42.12,
+ 
+      }
+    }
+  });
+
 const app = express();
+
 
 app.listen(3000, () => {
     console.log('Running GraphQL server...')
